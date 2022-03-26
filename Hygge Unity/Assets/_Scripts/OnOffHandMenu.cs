578b5a23409e7f0643b3e73b10113ac2c6239982
Joannehuang11 <chwong0314@me.com>
2022-03-25 21:36:34 -0400

    misc(*): Add interactive handmenu

---
+++ Hygge Unity/Assets/_Scripts/OnOffHandMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class OnOffHandMenu : MonoBehaviour
+{
+    public GameObject moodUI;
+    public bool activeMoodUI = true;
+    [SerializeField] ActionBasedController actionBasedController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayMoodUI();
+
+        actionBasedController.selectAction.action.performed += PressTrigger;
+        
+
+    }
+
+    public void PressTrigger(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            DisplayMoodUI();
+        }
+    }
+
+    public void DisplayMoodUI()
+    {
+        if(activeMoodUI)
+        {
+            moodUI.SetActive(false);
+            activeMoodUI = false;
+        }
+        else if (!activeMoodUI)
+        {
+            moodUI.SetActive(true);
+            activeMoodUI = true;
+        }
+    }
+}
